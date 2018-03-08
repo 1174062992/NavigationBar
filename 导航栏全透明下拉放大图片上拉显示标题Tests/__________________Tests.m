a038ee20e5cb4799f14f7ded6aefff9a87782b54
xunli <xunli@cmmcc.local>
2018-03-08 17:28:17 +0800

    Initial Commit

---
+++ 导航栏全透明下拉放大图片上拉显示标题Tests/__________________Tests.m
@@ -0,0 +1,39 @@
+//
+//  __________________Tests.m
+//  导航栏全透明下拉放大图片上拉显示标题Tests
+//
+//  Created by xunli on 2018/3/8.
+//  Copyright © 2018年 caoji. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __________________Tests : XCTestCase
+
+@end
+
+@implementation __________________Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
